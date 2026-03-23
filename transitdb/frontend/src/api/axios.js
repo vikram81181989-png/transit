1b05f293a34f7c775d5158c39c43737e10c2df20
LELLA VIKRAM HARISH <vikram81181989@gmail.com>
2026-03-23 21:22:40 +0530

    connect frontend to backend

--- transitdb/frontend/src/api/axios.js
+++ transitdb/frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://transit-vup0.onrender.com/api' });
 
 // Attach JWT to every request
 api.interceptors.request.use(cfg => {
